e96295f9f9456a08c67efe5f8bedd4eca622a9a2
Simone Sturniolo <simonesturniolo@gmail.com>
2025-12-13 17:31:34 +0000

    Added Ollama environment file

---
+++ ollama_env.sh
@@ -0,0 +1,5 @@
+# Environmental variables for Ollama server
+# Enable flash attention mechanism
+export OLLAMA_FLASH_ATTENTION=1
+export OLLAMA_KV_CACHE_TYPE="q8_0"
+export OLLAMA_CONTEXT_LENGTH=8192
